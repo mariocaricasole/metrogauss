49644d58aacc21689c2cbe2ff20a3d0a0dd5f906
mariocaricasole <caricasolemario@gmail.com>
2021-10-11 12:18:34 +0200

    removed redundancy

--- metrogauss_analysis.py
+++ metrogauss_analysis.py
@@ -1,29 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
 
 vals = np.loadtxt("values.txt")
 
 #evaluate mean and "normal" standard deviation
-mean = np.mean(vals[5000:])
-std = np.std(vals[5000:])
-
-#define autocorrelation function
-def C(k,sample):
-    N=len(sample)
-    array = np.zeros_like(k,dtype=np.float64)
-    for kk in k:
-        product1 = sample[:N-kk] - sample.mean()
-        product2 = sample[kk:] - sample.mean()
-        out=np.sum(product1*product2)/((N-kk)*sample.std())
-        array[kk-1] = out
-
-    return array
+mean = np.mean(vals[9000:])
+var = np.var(vals[9000:])
 
 #compute autocorrelation table up to M steps
 def autocorr_table(M,sample):
     N=len(sample)
-    kk = np.linspace(0,M,M,dtype=int)
-    CC = C(kk,sample)
+    k = np.linspace(0,M,M,dtype=int)
+    CC = np.zeros_like(k,dtype=np.float64)
+    for kk in k:
+        product1 = sample[:N-kk] - sample.mean()
+        product2 = sample[kk:] - sample.mean()
+        out=np.sum(product1*product2)/((N-kk)*sample.std())
+        CC[kk-1] = out
 
     return CC
 
@@ -38,7 +32,11 @@ for i in range(0,len(tau)-1):
 
 tau = np.asarray(tau)
 
+#evaluate corrected variance
+sigma_correct = var * (1+2*np.max(tau))/len(vals[9000:])
+print(f"Corrected variance: {sigma_correct}")
+
 #plot out
 plt.figure()
-plt.plot(tau)
+plt.plot(vals)
 plt.show()
